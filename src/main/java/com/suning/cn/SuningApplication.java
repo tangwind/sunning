48d067715d1676e1a7ab9d2ae44fd0099b4e7bcc
sht9808 <1711424366@qq.com>
2020-09-21 16:22:44 +0800

    init

---
+++ src/main/java/com/suning/cn/SuningApplication.java
@@ -0,0 +1,13 @@
+package com.suning.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suning/cn/SuningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suning.cn;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SuningApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
