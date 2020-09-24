ae7235bbd4a4e3b0290c84deef2fb82bc5af9dc6
Hardy-Mc <785020691@qq.com>
2020-09-24 11:42:58 +0800

    错误 待更改

--- src/main/java/com/suning/cn/service/CartService.java
+++ src/main/java/com/suning/cn/service/CartService.java
@@ -1,13 +1,20 @@
 package com.suning.cn.service;
 
+import com.suning.cn.dto.Cart;
 import com.suning.cn.vo.CartVo;
+
+import java.util.List;
 
 /**
  * Created by  lzy  on 2020/9/23 15:02
  */
 public interface CartService {
 
+    List<Cart> selectGoodsInfo(String userId);
+
     boolean addToCart(CartVo cartVo);
+
+    List<Cart> updateGoodsNum(CartVo... cartVo);
 
     boolean isDel(CartVo cartVo);
 
--- src/main/java/com/suning/cn/service/impl/CartServiceImpl.java
+++ src/main/java/com/suning/cn/service/impl/CartServiceImpl.java
@@ -11,6 +11,8 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * Created by  lzy  on 2020/9/23 15:02
  */
@@ -21,6 +23,15 @@ public class CartServiceImpl implements CartService {
 
     @Reference
     private CartMapper cartMapper;
+
+
+    @Override
+    public List<Cart> selectGoodsInfo(String userId) {
+        CartExample cartExample = new CartExample();
+        cartExample.createCriteria().andUserIdEqualTo(userId);
+        List<Cart> cartList = cartMapper.selectCartInfoByExample(cartExample);
+        return cartList;
+    }
 
     @Override
     public boolean addToCart(CartVo cartVo) {
@@ -34,14 +45,44 @@ public class CartServiceImpl implements CartService {
         return false;
     }
 
+    // 修改数量
+    // 不可大于库存量
+    // 数量不可小于1
+    @Override
+    public List updateGoodsNum(CartVo... cartVo) {
+
+
+        CartExample cartExample = new CartExample();
+        // 根据用户id获取商品数据 可能是一个集合！！
+        cartExample.createCriteria().andUserIdEqualTo(cart.getUserId());
+        List<Cart> cartList = cartMapper.selectCartInfoByExample(cartExample);
+        // 根据商品id 修改商品数量
+        cartMapper.updateNumByGoodId(cart.);
+        // 数量范围判断
+
+
+        cartExample.createCriteria();
+
+        cartMapper.updateByExampleSelective();
+        // 返回是否成功
+
+
+
+        return ;
+    }
+
     @Override
     public boolean isDel(CartVo cartVo) {
         Cart cart = new Cart();
         CartExample cartExample = new CartExample();
-
         BeanUtils.copyProperties(cartVo, cart);
+        cart.setIsDel(0);
+        cartExample.createCriteria().andUserIdEqualTo(cartVo.getUserId());
         // todo 逻辑删除！
-        //cartMapper.updateByExampleSelective(cartVo, );
+        int row = cartMapper.updateByExampleSelective(cart, cartExample);
+        if (row > 0) {
+            return true;
+        }
 
         return false;
     }
