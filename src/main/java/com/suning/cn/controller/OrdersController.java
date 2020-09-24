fa87e307263ff4d5c9dd14dfb5e0e3a274fecbf1
sht9808 <1711424366@qq.com>
2020-09-24 17:01:59 +0800

    onGoing  order部分提交

--- src/main/java/com/suning/cn/controller/OrdersController.java
+++ src/main/java/com/suning/cn/controller/OrdersController.java
@@ -1,54 +1,48 @@
 package com.suning.cn.controller;
 
+import com.suning.cn.params.GoodsParam;
 import com.suning.cn.params.OrderParam;
 import com.suning.cn.service.OrdersService;
 import com.suning.cn.utils.ReturnResult;
+import com.suning.cn.utils.ReturnResultUtils;
+import com.suning.cn.vo.OrderVo;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/suning")
-@Api
+@Api(tags = "订单接口")
 public class OrdersController {
 
     @Autowired
     private OrdersService ordersService;
 
-    /**
-     * 用户请求主页
-     * 返回参数1：类型（头部）
-     * 返回参数2：商品列表（主体）:商品名，商品大图，
-     */
-    public ReturnResult homePage(String typeId) {
-        return ordersService.homePage(typeId);
-    }
 
     /**
-     * 商品详情页
-     * <p>
-     * param goodsId 商品id
-     * 根据商品id获取商品信息
-     * 返回所获结果
-     */
-    public ReturnResult goodsDetail(String goodsId) {
-        return ordersService.getGoodsDetail(goodsId);
-    }
-
-    /**
-     * 生成待支付订单（单个支付/非购物车）
+     * 生成待支付订单（）
      * <p>
      * 根据参数生成待支付订单，返回结果
      * 前台需要：商品信息(店铺名)；订单：数量、总额；用户：收货地址；
      *
-     * @param orderParam 订单信息参数，包含goodsId，price，count
+     * @param goodsParams 订单信息参数，包含goodsId，price，count
      * @return
      */
+    @ApiOperation(value = "生成未支付订单",notes = "goodParams参数为对象数组")
     @PostMapping(value = "/confirm")
-    public ReturnResult generatorOrder(@RequestBody OrderParam orderParam) {
-        return ordersService.generatorOrder(orderParam);
+    public ReturnResult generatorOrder(@RequestParam String userId, @RequestBody GoodsParam... goodsParams) {
+        //return ordersService.generatorOrder(orderParam);
+        try {
+            return ordersService.generatorOrder(goodsParams, userId);
+        }catch (Exception e){
+            return ReturnResultUtils.returnFail(707,"订单生成失败");
+        }
     }
+
+    /**
+     * 购物车生成支付订单（多条支付）
+     *
+     * 参数包含：商品信息数组，携带对应商品数量，
+     */
 }
